673df74444e28ac77bea2be6a4f4b9a289377b18
Zdragun <nazar2001770@gmail.com>
2023-05-09 09:31:55 +0300

    replaced singlemovie with movie

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -7,9 +7,9 @@ const MovieList = ({ dataMovie, error, loading }) => {
         <main className='main-container'>
             {loading ? <h1 className='load'>Wait Data is Loading ...</h1> :
                 error ? <h1 className='error'>Sorry the error is occurred</h1> :
-                    dataMovie?.map((singlemovie) => (
-                        <div key={singlemovie.id}>
-                        <MovieCard singlemovie={singlemovie} />
+                    dataMovie?.map((movie) => (
+                        <div key={movie.id}>
+                        <MovieCard movie={movie} />
                         </div>
                     ))
             }
