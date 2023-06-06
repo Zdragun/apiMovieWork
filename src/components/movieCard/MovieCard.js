0e925806f1dfa60361492fc274852616c366312b
Zdragun <nazar2001770@gmail.com>
2023-06-06 20:04:53 +0300

    Deployed application

--- src/components/movieCard/MovieCard.js
+++ src/components/movieCard/MovieCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import '../MovieCard/MovieCard.css';
 import { AiOutlineStop } from 'react-icons/ai';
 import { MdOutlineVerifiedUser } from 'react-icons/md';
